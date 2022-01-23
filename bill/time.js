938ce32fdb9c20e20287a6f7e75bf6b3f9dfef95
jaldipbhalani <jaldipbhalani@gmail.com>
2022-01-23 17:44:40 +0530

    22-01-2022

---
+++ bill/time.js
@@ -0,0 +1,23 @@
+// const countdown = () => {
+            // const countDate = new Date("May 19, 2021 00:00:00").getTime();
+            // const now = new Date().getTime();
+            // const gap = countDate - now;
+
+            // const second = 1000;
+            // const minute = second * 60;
+            // const hour = minute * 60;
+            // const day = hour * 24;
+
+            // var textDay = gap / day;
+            // var textHour = Math.floor(Math.abs((gap % day) / hour));
+            // var textMinute = Math.floor(Math.abs((gap % hour) / minute));
+            // var textSecond = Math.floor(Math.abs((gap % minute) / second));
+            // // console.log(textDay);
+            // // console.log(textHour);
+            // // console.log(textMinute);
+            // // console.log(textSecond);
+            // document.getElementById("hour").innerHTML=textHour
+            // document.getElementById("minit").innerHTML=textMinute
+            // document.getElementById("sec").innerHTML=textSecond
+            // };
+            // setInterval(countdown, 1000);

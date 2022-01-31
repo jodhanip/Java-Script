432e52ead72fea9558135d6aa1b69eb1c73d89cb
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-01-31 12:57:20 +0530

    31-1-2022

---
+++ snake/extra.js
@@ -0,0 +1,21 @@
+let arr = ["jaldip",45,98,{hi:89,2:"jal"}]
+let a=[],j=0
+// console.log(arr)
+// console.log(...arr)
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+    // console.log({...arr})
+    a={...arr[i]}
+    console.log(a)
+    // a=[...arr]
+    // console.log{...arr[i]}
+    // console.log(arr[2])
+    // console.log(arr)
+//     j++
+}
+// console.log(a)
+// for (let j = 0; j < j.length; j++) {
+    
+//     console.log(a[j])
+    
+// }
